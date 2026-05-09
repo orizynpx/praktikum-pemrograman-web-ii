38d7c3678c4895e45425b429ecabd95490f5c191
orizynpx <71197340+orizynpx@users.noreply.github.com>
2026-05-09 14:24:06 +0800

    feat: implement PRAK303

---
+++ Modul 3/PRAK303.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PRAK303</title>
+    <style>
+        .star {
+            width: 18px;
+            height: 18px;
+        }
+    </style>
+</head>
+
+<body>
+    <form method="post">
+        <label>Batas Bawah : <input type="text" name="lower_limit" value="<?= $_POST['height'] ?? '' ?>"></label><br>
+        <label>Batas Atas : <input type="text" name="upper_limit" value="<?= $_POST['image_url'] ?? '' ?>"></label><br>
+        <button type="submit" name="print">Cetak</button><br>
+        <br>
+    </form>
+
+    <?php
+    if (isset($_POST['print'])) {
+        $lower_limit = $_POST['lower_limit'];
+        $upper_limit = $_POST['upper_limit'];
+
+        do {
+            if (($lower_limit + 7) % 5 == 0) {
+                echo '<img src="star.png" class="star"> ';
+            } else {
+                echo $lower_limit . ' ';
+            }
+            $lower_limit++;
+        } while ($lower_limit <= $upper_limit);
+    }
+    ?>
+</body>
+
+</html>
