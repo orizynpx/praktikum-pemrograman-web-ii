dc062c4c7f579739d3280a237f86e606147f5587
orizynpx <71197340+orizynpx@users.noreply.github.com>
2026-06-03 20:06:45 +0800

    fix: remove commented line

--- Modul 6/PRAK601/app/Config/Routes.php
+++ Modul 6/PRAK601/app/Config/Routes.php
@@ -4,5 +4,4 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Pages::index');
-// $routes->get('/home', 'ProfileController::index');
 $routes->get('/profile', 'Pages::profile');
