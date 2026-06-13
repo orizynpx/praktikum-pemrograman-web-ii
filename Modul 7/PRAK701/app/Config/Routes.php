869c6bc073700e1b256da0a5180d70c64b26da4b
orizynpx <71197340+orizynpx@users.noreply.github.com>
2026-06-13 11:51:28 +0800

    feat: update routes to include authentication and book management

--- Modul 7/PRAK701/app/Config/Routes.php
+++ Modul 7/PRAK701/app/Config/Routes.php
@@ -2,5 +2,20 @@
 
 use CodeIgniter\Router\RouteCollection;
 
-/** @var RouteCollection $routes */
-$routes->get('/', 'Home::index');
+/**
+ *  @var RouteCollection $routes
+ */
+
+$routes->get('/', 'AuthController::index');
+$routes->get('/login', 'AuthController::index');
+$routes->post('/login', 'AuthController::login');
+$routes->get('/logout', 'AuthController::logout');
+
+$routes->group('buku', ['filter' => 'auth'], function ($routes) {
+    $routes->get('/', 'BukuController::index');
+    $routes->get('create', 'BukuController::create');
+    $routes->post('store', 'BukuController::store');
+    $routes->get('edit/(:num)', 'BukuController::edit/$1');
+    $routes->post('update/(:num)', 'BukuController::update/$1');
+    $routes->get('delete/(:num)', 'BukuController::delete/$1');
+});
